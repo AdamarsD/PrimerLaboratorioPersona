439b0b7d92aa3322c812b7c0e54c454114065dd7
AdamarsD <ad0094032021@unab.edu.sv>
2021-08-23 19:46:07 -0600

    Negocio

--- Persona/Persona/Negocio/NClsPersona.cs
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
+        public String Hablar(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " esta hablando ";
+
+        }
+
+        public String Caminarr(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " esta caminando ";
+
+        }
+
+        public String Reirr(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " esta riendo ";
+
+        }
+
+        public String Observar(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " esta observando ";
+
+        }
     }
 }
